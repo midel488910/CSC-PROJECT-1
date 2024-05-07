116ef42495796c9076ed99b7bc996e56ccc708e9
John Angelo Midel <j.midel.488910@umindanao.edu.ph>
2024-05-07 11:19:22 +0800

    added CaseFile Model

--- CSC_File_Manager/main/models.py
+++ CSC_File_Manager/main/models.py
@@ -2,6 +2,13 @@ from django.db import models
 from django.contrib.auth.models import User
 # Create your models here.
 
+class CaseFile(models.Model):
+    title = models.CharField(max_length=50)
+    filePdf = models.FileField(upload_to='files')
+    created_at = models.DateTimeField(auto_now_add=True)
+    
+    
+
 
 class Post(models.Model):
     author = models.ForeignKey(User, on_delete=models.CASCADE)
