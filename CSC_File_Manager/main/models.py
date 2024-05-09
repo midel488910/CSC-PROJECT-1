7b67b157ab60333e404ed0997a02a8a331d315d6
John Angelo Midel <j.midel.488910@umindanao.edu.ph>
2024-05-09 15:24:51 +0800

    python magic

--- CSC_File_Manager/main/models.py
+++ CSC_File_Manager/main/models.py
@@ -3,12 +3,19 @@ from django.contrib.auth.models import User
 import os
 from datetime import datetime
 from django.core.validators import FileExtensionValidator
+import magic
 
  #validator that accepts only pdf files
 ext_validator = FileExtensionValidator(['pdf'])
+def validate_file_mimetype(file):
+    accept = ['application/pdf']
+    file_mine_type = magic.from_buffer(file.read(1024),mime=True)
+    if file_mine_type not in accept:
+        raise ValidationError("Unsupported File Type")
 
-# Create your models here.
 
+
+#Database columns for PDF Files 
 class CaseFile(models.Model):
     author = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
     title = models.CharField(max_length=50)
@@ -16,30 +23,23 @@ class CaseFile(models.Model):
     docket_number = models.CharField(max_length=50,blank=True)
     respondents = models.CharField(max_length=50,blank=True)
     remarks = models.CharField(max_length=50,blank=True)
-    filePdf = models.FileField(upload_to='files',blank=False,validators=ext_validator)
+    filePdf = models.FileField(upload_to='files',blank=False,validators=[ext_validator,validate_file_mimetype])
     created_at = models.DateTimeField(auto_now_add=True)
+
     def __str__(self):
         return self.title   
     
+    #when delete is clicked the file is also removed from the database
     def delete(self, *args, **kwargs):
         # Delete the file from the media directory
         if self.filePdf:
             os.remove(self.filePdf.path)
-
         # Call the parent class's delete method
         super().delete(*args, **kwargs)
     
 
 
-class Post(models.Model):
-    author = models.ForeignKey(User, on_delete=models.CASCADE)
-    title = models.CharField(max_length=50)
-    description = models.TextField()
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
 
-    def __str__(self):
-        return self.title + "\n" + self.description
     
 
 
