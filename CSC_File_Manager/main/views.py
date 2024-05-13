6426d4a9fa99a3a2e78c57e7daf4991dc4c9f81a
John Angelo Midel <j.midel.488910@umindanao.edu.ph>
2024-05-13 08:50:17 +0800

    Additional Changes

--- CSC_File_Manager/main/views.py
+++ CSC_File_Manager/main/views.py
@@ -1,12 +1,10 @@
-from django.contrib import messages
+
 from django.shortcuts import render, redirect
-from django.http import HttpResponse, HttpResponseRedirect
 from django.contrib.auth import login,logout,authenticate
 from .forms import RegisterForm, CaseForm
 from django.contrib.auth.decorators import login_required, permission_required
 from django.views.decorators.csrf import csrf_exempt, csrf_protect
 from .models import CaseFile
-from django.views import generic
 from django.db.models import Q
 
 
